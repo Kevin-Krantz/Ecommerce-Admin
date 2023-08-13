c895b198beb6e426ac2a8c6de5407969881f8979
Kevin <kevinkrantz95@hotmail.com>
2023-08-13 10:31:40 +0200

    feat: Implement mongoose-based CRUD for products
    - Initialize mongoose connection in API routes.
    
    - Add POST endpoint to create a product.
    
    - Implement GET endpoint to retrieve products by id or list all.
    
    - Introduce PUT endpoint to update product details.

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,8 +1,39 @@
-import { NextResponse } from "next/server";
+import { mongooseConnect } from "@/lib/mongoose";
+import Product from "@/models/Product";
+import { _id } from "@next-auth/mongodb-adapter";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: Request) {
-  return NextResponse.json(req.method);
-  // const { method } = req;
-  // if (method === "POST") {
-  // }
+export async function POST(req: NextRequest) {
+  await mongooseConnect();
+  const data = await req.json();
+  const { title, description, price } = data;
+
+  const productDoc = await Product.create({
+    title,
+    description,
+    price,
+  });
+
+  return NextResponse.json(productDoc);
+}
+
+export async function GET(req: NextRequest) {
+  await mongooseConnect();
+
+  const url = new URL(req.url);
+  const id = url.searchParams.get("id");
+
+  if (id) {
+    return NextResponse.json(await Product.findById(id));
+  } else return NextResponse.json(await Product.find());
+}
+
+export async function PUT(req: NextRequest) {
+  await mongooseConnect();
+
+  const data = await req.json();
+  const { title, description, price, _id } = data;
+  return NextResponse.json(
+    await Product.updateOne({ _id }, { title, description, price })
+  );
 }
