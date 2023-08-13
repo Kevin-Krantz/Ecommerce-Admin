5931cf7ae1240aacecc36a78867772b90a9f858c
Kevin <kevinkrantz95@hotmail.com>
2023-08-13 11:29:35 +0200

    feat: Enhance Products page to display list of products
    - Integrate state management to handle products list.
    
    - Fetch products from /api/products using axios.
    
    - Display products in a table with edit and delete options.

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -2,8 +2,22 @@
 
 import Link from "next/link";
 import Layout from "@/app/components/Layout";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface IProductWithID extends IProduct {
+  _id: string;
+}
 
 export default function Products(): JSX.Element {
+  const [products, setProducts] = useState<IProductWithID[]>([]);
+
+  useEffect(() => {
+    axios.get("/api/products").then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
+
   return (
     <Layout>
       <Link
@@ -12,6 +26,41 @@ export default function Products(): JSX.Element {
       >
         Add new product
       </Link>
+      <table className="basic mt-2">
+        <thead>
+          <tr>
+            <td>Product name</td>
+            <td></td>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr key={product._id}>
+              <td>{product.title}</td>
+              <td>
+                <Link href={`/products/edit/${product._id}`}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-4 h-4"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+                    />
+                  </svg>
+                  Edit
+                </Link>
+                <Link href={`/products/delete/${product._id}`}>Delete</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </Layout>
   );
 }
