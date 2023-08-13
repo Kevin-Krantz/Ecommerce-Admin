1e3319d9214672ee476031cdd4e4489f41079938
Kevin <kevinkrantz95@hotmail.com>
2023-08-13 11:32:10 +0200

    refactor: Use ProductsForm in NewProduct component
    - Replace direct form implementation with reusable ProductsForm component.
    
    - Remove local state management and axios call, delegating product creation to ProductsForm.

--- app/products/new/page.tsx
+++ app/products/new/page.tsx
@@ -1,48 +1,13 @@
 "use client";
 
 import Layout from "@/app/components/Layout";
-import axios from "axios";
-import { useState } from "react";
+import ProductsForm from "@/app/components/ProductsForm";
 
 export default function NewProduct(): JSX.Element {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [price, setPrice] = useState("");
-  async function createProduct(
-    ev: React.FormEvent<HTMLFormElement>
-  ): Promise<void> {
-    ev.preventDefault();
-    const data = { title, description, price };
-    await axios.post("/api/products", data);
-  }
   return (
     <Layout>
-      <form onSubmit={createProduct}>
-        <h1>New Product</h1>
-        <label>Product name</label>
-        <input
-          type="text"
-          placeholder="product name"
-          value={title}
-          onChange={(ev) => setTitle(ev.target.value)}
-        />
-        <label>Description</label>
-        <textarea
-          placeholder="description"
-          value={description}
-          onChange={(ev) => setDescription(ev.target.value)}
-        />
-        <label>Price (in USD)</label>
-        <input
-          type="number"
-          placeholder="price"
-          value={price}
-          onChange={(ev) => setPrice(ev.target.value)}
-        />
-        <button type="submit" className="btn-primary">
-          Save
-        </button>
-      </form>
+      <h1>New Product</h1>
+      <ProductsForm title="" description="" price={0} />
     </Layout>
   );
 }
