d92769de304b49f2c0ab80ee553881da54b3bda2
Kevin <kevinkrantz95@hotmail.com>
2023-09-07 05:23:50 +0200

    Update CRUD operations for additional fields and error handling
    - Extend POST method to support 'category' and 'properties' fields
    
    - Refactor PUT method to conditionally update 'category' and 'properties'
    
    - Add error handling in PUT method for update failures
    
    These changes allow the Product model to be more flexible and robust in handling different kinds of updates.

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -6,15 +6,21 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   await mongooseConnect();
   const data = await req.json();
-  const { title, description, price, images } = data;
+  const { title, description, price, images, category, properties } = data;
 
-  const productDoc = await Product.create({
+  const newProductData: Partial<IProduct> = {
     title,
     description,
     price,
     images,
-  });
+  };
 
+  if (category !== "") {
+    newProductData.category = category;
+    newProductData.properties = properties;
+  }
+
+  const productDoc = await Product.create(newProductData);
   return NextResponse.json(productDoc);
 }
 
@@ -33,10 +39,35 @@ export async function PUT(req: NextRequest) {
   await mongooseConnect();
 
   const data = await req.json();
-  const { title, description, price, images, _id } = data;
-  return NextResponse.json(
-    await Product.updateOne({ _id }, { title, description, price, images })
-  );
+  const { title, description, price, images, _id, category, properties } = data;
+
+  const updateFields: Partial<IProduct> = {
+    title,
+    description,
+    price,
+    images,
+  };
+
+  let updateOperation: {
+    $set: Partial<IProduct>;
+    $unset?: Partial<Record<keyof IProduct, string>>;
+  } = {
+    $set: updateFields,
+  };
+
+  if (category === "") {
+    updateOperation["$unset"] = { category: "", properties: "" };
+  } else {
+    updateFields.category = category;
+    updateFields.properties = properties;
+  }
+
+  try {
+    await Product.updateOne({ _id }, updateOperation);
+    return NextResponse.json(await Product.findById(_id));
+  } catch (error) {
+    return NextResponse.json({ error: "Could not update product" });
+  }
 }
 
 export async function DELETE(req: NextRequest) {
