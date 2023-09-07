7454cec13777b22d899fe436c7509cce979dbbd5
Kevin <kevinkrantz95@hotmail.com>
2023-09-07 05:20:59 +0200

    Add CRUD operations for Category model
    - Implement POST method for creating a new category
    
    - Implement GET method for reading categories (by ID or all)
    
    - Implement PUT method for updating a category by ID
    
    - Implement DELETE method for removing a category by ID
    
    The methods handle MongoDB operations and return appropriate responses.

---
+++ app/api/categories/route.ts
@@ -0,0 +1,63 @@
+import { mongooseConnect } from "@/lib/mongoose";
+import Category from "@/models/Category";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  await mongooseConnect();
+  const data = await req.json();
+  let { name, parentCategory, properties } = data;
+
+  if (parentCategory === "") {
+    parentCategory = undefined;
+  }
+
+  const categoryDoc = await Category.create({
+    name,
+    parent: parentCategory || undefined,
+    properties,
+  });
+  return NextResponse.json(categoryDoc);
+}
+
+export async function GET(req: NextRequest) {
+  await mongooseConnect();
+
+  const url = new URL(req.url);
+  const id = url.searchParams.get("id");
+
+  if (id) {
+    return NextResponse.json(await Category.findById(id));
+  } else return NextResponse.json(await Category.find().populate("parent"));
+}
+
+export async function PUT(req: NextRequest) {
+  await mongooseConnect();
+  const data = await req.json();
+  let { name, parentCategory, _id, properties } = data;
+
+  if (parentCategory === "") {
+    parentCategory = undefined;
+  }
+
+  const categoryDoc = await Category.updateOne(
+    { _id },
+    {
+      name,
+      parent: parentCategory,
+      properties,
+    }
+  );
+  return NextResponse.json(categoryDoc);
+}
+
+export async function DELETE(req: NextRequest) {
+  await mongooseConnect();
+
+  const url = new URL(req.url);
+  const _id = url.searchParams.get("_id");
+
+  console.log(url);
+
+  await Category.deleteOne({ _id });
+  return NextResponse.json("SUCCESSFULLY DELETED CATEGORY");
+}
