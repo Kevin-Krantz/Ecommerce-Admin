a92a003604623326a42b375f1ed3f4e8c6ce9245
Kevin Krantz <kevinkrantz95@hotmail.com>
2024-05-06 13:00:30 +0200

    Add timestamps to Product model

--- models/Product.ts
+++ models/Product.ts
@@ -2,14 +2,19 @@ import mongoose, { Schema, Document, model, Model, models } from "mongoose";
 
 interface IProductDocument extends IProduct, Document {}
 
-const ProductSchema = new Schema<IProductDocument>({
-  title: { type: String, required: true },
-  description: String,
-  price: { type: Number, required: true },
-  images: [{ type: String }],
-  category: { type: mongoose.Types.ObjectId, ref: "Category" },
-  properties: { type: Object },
-});
+const ProductSchema = new Schema<IProductDocument>(
+  {
+    title: { type: String, required: true },
+    description: String,
+    price: { type: Number, required: true },
+    images: [{ type: String }],
+    category: { type: mongoose.Types.ObjectId, ref: "Category" },
+    properties: { type: Object },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 let Product: Model<IProductDocument>;
 
